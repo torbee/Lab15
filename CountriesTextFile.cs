12f19aa60bd3c3ccc2da690c8b0198bf1c84b4e2
torbee <vkishman88@gmail.com>
2017-07-13 19:52:48 -0400

    Add files via upload

---
+++ CountriesTextFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab15
+{
+    public class CountriesTextFile
+    {
+        public void Reader() {
+            string filepath = @"C:\Users\GC Student\Documents\Visual Studio 2015\Projects\Lab15\Lab15\countries.txt";
+            StreamReader reader = new StreamReader(filepath);
+            string input;
+
+            string line = reader.ReadLine();
+            input = reader.ReadLine();
+            Console.WriteLine(input);
+            Console.WriteLine(line);
+        }
+        
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Countries Maintenance Application!");
+            Console.WriteLine("1 - See the list of countries.");
+            Console.WriteLine("2 - Add a country.");
+            Console.WriteLine("3 - Exit.");
+
+            Reader a = new Reader();
+            Console.WriteLine(a);
+        }
+    }
+}
